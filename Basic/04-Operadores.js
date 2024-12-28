bc7d3dbaa71b5d9770b9f52cd966d7521bee3784
PaulLopez12 <paul.lopez.b@uni.pe>
2024-12-28 16:22:30 -0500

    operadores hasta mayor y menor

---
+++ Basic/04-Operadores.js
@@ -0,0 +1,38 @@
+//Operadores
+
+//Operadores Aritméticos
+let a = 5;
+let b = 10;
+console.log(a+b) //Suma
+console.log(a-b) //Resta
+console.log(a*b) //Multiplicación
+console.log(a/b) //División
+
+console.log(a%b) //Módulo
+console.log(a**b) //Potencia
+
+a++
+console.log(a) //Incremento
+
+b--
+console.log(b) //Decremento
+
+//Operadores de Asignación
+let myVar = 2;
+console.log(myVar);
+myVar += 2;
+console.log(myVar);
+
+myVar -= 2;
+myVar *= 2;
+myVar /= 2;
+myVar %= 2;
+myVar **= 2;
+
+//Operadores de Comparación
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a==b) 
+console.log(a!=b)  
