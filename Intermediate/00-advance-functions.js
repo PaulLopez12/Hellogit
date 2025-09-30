f2faaa272d221bbbc1b24b51d12587dd9406f59b
PaulLopez12 <paul.lopez.b@uni.pe>
2025-09-30 18:48:32 -0500

    Funciones avanzadas

--- Intermediate/00-advance-functions.js
+++ Intermediate/00-advance-functions.js
@@ -51,4 +51,83 @@ handler.arrowgreeting();
     console.log("IIFE con arrow function")
 })();
 
-//Parametros Rest
+//Parametros Rest (...) genera un array con los argumentos que se le pasen a la funcion
+function sum(...numbers) {
+        let result = 0
+        for (let number of numbers) {
+            result += number
+        }
+        return result
+    }
+console.log(sum(1,2,3,4,5))
+console.log(sum(10,20,30))
+
+//Operador Spread (...) expande un array en elementos individuales
+const numbers = [1,2,3,4,5]
+function sumWithSpread(a,b,c,d,e) {
+    return a + b + c + d + e
+}
+console.log(sumWithSpread(1,2,3,4,5))//Sin spread
+console.log(sumWithSpread(...numbers))//Con spread
+
+//Closures (una funcion que recuerda el entorno en el que fue creada)
+function createCounter(){
+    let counter = 0
+    return function(){
+        counter++
+        console.log(`Contador: ${counter}`) 
+    }  
+}
+const counter = createCounter()
+counter()
+counter()
+
+const counter2 = createCounter()
+counter2()
+counter2()
+
+//Recursividad (una funcion que se llama a si misma)
+function factorial(n) {
+    if (n <= 1) {
+        return 1
+    }
+    return n * factorial(n-1)
+}
+console.log(factorial(5)) // 120
+
+//Funciones parciales (crear una nueva funcion con algunos argumentos predefinidos)
+function partialSum(a) {
+    return function(b,c) {
+        return sum(a,b,c)
+    }
+}
+const sumWith = partialSum(10)
+console.log(sumWith(20,30))
+
+//Currying (transformar una funcion con multiples argumentos en una secuencia de funciones que reciben un solo argumento)
+function currySum(a) {
+    return function(b) {
+        return function(c) {
+            return sum(a,b,c)
+        }
+    }
+}
+
+console.log(currySum(10)(20)(30)) // 60
+const add10 = currySum(10)
+const add10And20 = add10(20)
+console.log(add10And20(30)) // 60
+
+//Callback functions (una funcion que se pasa como argumento a otra funcion y se ejecuta dentro de esa funcion)
+function processData(data, callback) {
+    const result = sum(...data)
+    callback(result)
+}
+
+function processResut(result){
+    console.log(`El resultado es: ${result}`)
+}
+processData([1,2,3,4,5],processResut)
+processData([1,2,3,4,5], (result) => {
+    console.log(`El resultado con arrow function es: ${result}`)
+})
