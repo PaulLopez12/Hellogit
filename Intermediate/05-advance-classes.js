b5f02902ef64a65dc7522f15bdf7d256f62fca0c
PaulLopez12 <paul.lopez.b@uni.pe>
2025-10-07 15:48:26 -0500

    Ejercicios objetos y clases por resolver

--- Intermediate/05-advance-classes.js
+++ Intermediate/05-advance-classes.js
@@ -89,4 +89,77 @@ const dragon = new Dragon("Dragon")
 console.log(dragon.name)
 dragon.fly() 
 
-// - Singleton
+// - Patrón Singleton (para tener una sola instancia de una clase)
+class Session {
+    constructor(name){
+        if(Session.instance){
+            return Session.instance
+        }
+        this.name = name
+        Session.instance = this // Guardamos la instancia
+    }
+}
+
+const sesssion1 = new Session("ZWarkingZ")
+const session2 = new Session("")
+console.log(sesssion1)
+console.log(session2)
+console.log(sesssion1 === session2) // true, ambas variables apuntan a la misma instancia
+
+// - Symbol
+const ID = Symbol('id')
+
+class User {
+    constructor(name){
+        this.name = name
+        this[ID] = Math.random()
+    }
+    getID(){
+        return this[ID]
+    }
+}
+
+const user = new User("Paul")
+console.log(user.name)
+console.log(user.ID) // undefined, no se puede acceder directamente
+console.log(user[ID])
+user[ID] = 123 // Se modifica el valor del Symbol
+console.log(user[ID])
+console.log(user.getID()) // Accedemos al valor del Symbol a través de un método
+
+// - instanceof
+class Car {
+     
+}
+const car = new Car()
+
+console.log(car instanceof Car)
+
+// create
+const car2 = Object.create(Car.prototype)
+console.log(car2 instanceof Car)
+
+// - proxy
+const proxy = {
+    get(target, property){
+        console.log('Accediendo a la propiedad:', property)
+        return target[property]
+    },
+    set(target, property, value){
+        if(property === 'balance' && value < 0){
+            throw new Error("El balance no puede ser negativo")
+        }
+        target[property] = value
+    }
+}
+
+class BankAccount {
+    constructor(balance){
+        this.balance = balance
+    }
+}
+
+const account = new Proxy(new BankAccount(100), proxy)
+console.log(account.balance)
+account.balance = 200
+console.log(account.balance)
