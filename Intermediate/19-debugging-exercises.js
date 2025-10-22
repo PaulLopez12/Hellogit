418af31abb18c785837e514047d90b764f2d9747
PaulLopez12 <paul.lopez.b@uni.pe>
2025-10-22 13:19:52 -0500

    debugging-exercises => terminado

--- Intermediate/19-debugging-exercises.js
+++ Intermediate/19-debugging-exercises.js
@@ -1,3 +1,12 @@
 // 1. Crea un código con un error lógico y usa VS Code para encontrarlo
+function powerError(a, b){
+    if((typeof a === "number") && (typeof b === "number") ){
+        return a**b
+    }else{
+        throw Error("Los parámetros deben ser números")
+    }
 
+}
+(powerError(0,"b"))
 // 2. Experimenta con breakpoints y observa cómo cambia el flujo de ejecución
+/*done*/ 
